80ed6f464f20e93688cff3f9ef08d13698af90f4
Rollin Francois <rfrancois21@hotmail.com>
2020-04-23 12:36:01 -0400

    1st

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Destini/Model/StoryLogic.swift
@@ -0,0 +1,9 @@
+//
+//  StoryLogic.swift
+//  Destini
+//
+//  Created by Rollin Francois on 4/23/20.
+//  Copyright © 2020 Francois Technology. All rights reserved.
+//
+
+import Foundation
